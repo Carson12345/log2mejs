95c6db811200749158db4ad6672e34c33e5aa78c
Carson Locke Ross <carson.yaumy@gmail.com>
2022-05-01 19:08:51 +0800

    feat: error handler

--- src/log2me.js
+++ src/log2me.js
@@ -155,25 +155,29 @@ var Log2MeJS = function (config) {
 
 // Only run if in debug mode
 if (l2mDebugId) {
-    window.addEventListener('DOMContentLoaded', function () {
-        let l2m = Log2MeJS();
-        if (l2mReceiveMode === 'web_rtc' && l2mPID) {
-            l2m.loadPeerJS(function(){
-                console.log(l2mPID);
-                var peer = new Peer();
-                peer.on('open', function(id) {
-                    console.log('My peer ID is: ' + id);
-                    const conn = peer.connect(l2mPID);
-                    conn.on('open', function() {
-                        console.log("Started Connection ...");
-                        l2m.init({
-                            rtcConn: conn
+    try {
+        window.addEventListener('DOMContentLoaded', function () {
+            let l2m = Log2MeJS();
+            if (l2mReceiveMode === 'web_rtc' && l2mPID) {
+                l2m.loadPeerJS(function(){
+                    console.log(l2mPID);
+                    var peer = new Peer();
+                    peer.on('open', function(id) {
+                        console.log('My peer ID is: ' + id);
+                        const conn = peer.connect(l2mPID);
+                        conn.on('open', function() {
+                            console.log("Started Connection ...");
+                            l2m.init({
+                                rtcConn: conn
+                            });
                         });
                     });
-                });
-            })
-        } else {
-            l2m.init();
-        }
-    });
+                })
+            } else {
+                l2m.init();
+            }
+        });   
+    } catch (error) {
+        console.log("Unable to init Log2MeJS");
+    }
 }
